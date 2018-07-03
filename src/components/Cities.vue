e158dd5aca32722464997b7eaf8566b51dfe3342
treipatru <andrei@planet34.org>
2018-07-03 23:14:50 +0200

    Updated list to use filter * List now displays conditionally using the searchbox * Using fuse.js

--- src/components/Cities.vue
+++ src/components/Cities.vue
@@ -1,8 +1,18 @@
 <template>
   <div class="">
+      <vue-fuse
+          :keys="keys"
+          :list="citiesData"
+          :defaultAll="true"
+          :threshold="0.2"
+          event-name="fuseResultsUpdated"
+      >
+      </vue-fuse>
+
       <div
           class="bracket-container"
           v-for="bracket, index in brackets"
+          v-if="shouldRenderBracket(index)"
           :key="index"
       >
           <h2
@@ -11,9 +21,10 @@
               {{bracket}}
           </h2>
 
-          <div class="city"
-               v-for="city,index in filteredCities[index]"
-               :key="index"
+          <div
+              class="city"
+              v-for="city,el in filteredCities[index]"
+              :key="el"
           >
               <p>{{city.cost}} - {{city.area}}, {{city.country}}</p>
           </div>
@@ -31,7 +42,7 @@ export default {
       return vm.brackets.map((val, index) => {
         let bracketCities = [];
 
-        for (let city of vm.citiesData) {
+        for (let city of vm.results) {
           const next = vm.brackets[index + 1];
           // It's a bit dense because we're doing multiple checks to eliminate
           // redundant iterations.
@@ -49,11 +60,27 @@ export default {
       });
     },
   },
+
+  created: function () {
+    this.$on('fuseResultsUpdated', (results) => {
+      this.results = results;
+    });
+  },
+
   data: function () {
     return {
       brackets: [100, 200, 300, 400, 500, 600, 700, 800, 900, 1000, 1250, 1500, 2000, 3000],
+      keys: ['cost', 'area', 'continent', 'country'],
+      results: [],
     };
   },
+
+  methods: {
+    shouldRenderBracket: function (i) {
+      return this.filteredCities[i].length !== 0;
+    },
+  },
+
   props: {
     citiesData: {
       type: Array,
