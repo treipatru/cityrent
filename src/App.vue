90c0ccd6ef91c7d6d9e0e872f8cc1b7f6abc3d70
treipatru <andrei@planet34.org>
2018-07-04 23:58:39 +0200

    Updated styles

--- src/App.vue
+++ src/App.vue
@@ -22,16 +22,10 @@ export default {
 </script>
 
 <style lang="scss">
-html, body {
-  margin: 0;
-  padding: 0;
-}
-
 #app {
-  font-family: 'Avenir', Helvetica, Arial, sans-serif;
+  font-family: 'Roboto', Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
+  color: #424242;
 }
 </style>
