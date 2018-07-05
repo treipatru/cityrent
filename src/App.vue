9ff6e1117ed2a1e26ac60bc5a274e617d7135dfb
treipatru <andrei@planet34.org>
2018-07-05 23:30:34 +0200

    First pass header functionality

--- src/App.vue
+++ src/App.vue
@@ -1,6 +1,28 @@
 <template>
-  <div id="app">
-    <Cities :citiesData="citiesData"/>
+  <div
+    id="app"
+    class="grid"
+  >
+    <header
+      v-bind:class="['col-12', 'grid-middle-noGutter', { 'small' : searched}]"
+    >
+
+      <transition name="fade">
+        <div
+          class="title col-6_sm-10"
+          data-push-left="off-3_sm-1"
+          v-if="!searched"
+        >
+          <h1>cityrent</h1>
+          <p class="tagline">Average monthly rent costs from 540 cities around the world.</p>
+        </div>
+      </transition>
+    </header>
+
+    <Cities
+      :citiesData="citiesData"
+      v-on:hasSearchStr="hasSearchStr"
+    />
   </div>
 </template>
 
@@ -16,21 +38,53 @@
     data: function () {
       return {
         citiesData: json.data,
+        searched: false,
       };
+    },
+    methods: {
+      hasSearchStr: function (val) {
+        this.searched = val;
+      }
     },
   };
 </script>
 
 <style lang="scss">
-  body {
+  body, #app {
     background: #FAFAFC;
     margin: 0;
-  }
-
-  #app {
     font-family: 'Roboto', Helvetica, Arial, sans-serif;
     -webkit-font-smoothing: antialiased;
     -moz-osx-font-smoothing: grayscale;
     color: #424242;
+  }
+
+  .fade-enter-active, .fade-leave-active {
+    transition: opacity 200ms;
+  }
+  .fade-enter, .fade-leave-to {
+    opacity: 0;
+  }
+
+  header {
+    min-height: 50vh;
+    transition: min-height 350ms ease-out;
+
+    &.small {
+      min-height: 0;
+    }
+
+    .title {
+      h1 {
+        margin: 0;
+        padding: 0;
+      }
+      .name {
+        font-size: 2rem;
+      }
+      .tagline {
+        font-size: 1rem;
+      }
+    }
   }
 </style>
--- src/components/Cities.vue
+++ src/components/Cities.vue
@@ -1,45 +1,55 @@
 <template>
-  <div class="grid">
-    <div class="col-12 toolbar grid-noGutter">
-        <div
-          class="col-4_sm-10"
-          data-push-left="off-4_sm-1"
+  <div
+    class="col-12"
+    id="cities-cont"
+  >
+    <div
+      v-bind:class="['toolbar', 'grid-noGutter', {'is-fixed' : isFixed}, {'is-fixed' : searched}]"
+    >
+      <div
+        class="col-6_sm-10"
+        data-push-left="off-3_sm-1"
+      >
+        <vue-fuse
+          class="searchbox"
+          ref="searchBox"
+          :keys="keys"
+          :list="citiesData"
+          :threshold="0.2"
+          :tokenize="true"
+          :matchAllTokens="true"
+          :findAllMatches="true"
+          :minMatchCharLength="3"
+          :location="0"
+          :distance="100"
+          :maxPatternLength="32"
+          inputChangeEventName="updatedSearch"
+          eventName="fuseResultsUpdated"
+          placeholder="Search for places..."
         >
-          <vue-fuse
-            class="searchbox"
-            :keys="keys"
-            :list="citiesData"
-            :threshold="0.2"
-            :tokenize="true"
-            :matchAllTokens="true"
-            :findAllMatches="true"
-            :minMatchCharLength="3"
-            :location="0"
-            :distance="100"
-            :maxPatternLength="32"
-            eventName="fuseResultsUpdated"
-          >
-          </vue-fuse>
-        </div>
+        </vue-fuse>
+      </div>
     </div>
 
 
-    <div class="col-12">
+    <div
+      v-bind:class="['bracket-wrapper', {'is-fixed' : isFixed}]"
+    >
       <div
         v-bind:class="['bracket-container', 'grid', 'lvl-' + (index + 1)]"
         v-for="(bracket, index) in brackets"
         :key="index"
         v-if="shouldRenderBracket(index)"
       >
-        <div class="col-1_xs-2">
+        <div class="col-1_xs-2_sm-2">
           <h2
             class="bracket-price"
           >
-            {{bracket}}
+            ${{bracket}}
           </h2>
         </div>
 
-        <div class="col-11_xs-10 grid">
+        <div class="col-11_xs-10_sm-10 grid">
           <div
             :class="['city', 'col-4_xs-12_sm-6']"
             v-for="(city,el) in filteredCities[index]"
@@ -61,19 +71,21 @@
 </template>
 
 <script>
+  import _throttle from 'lodash.throttle';
+
   export default {
     name: 'Cities',
 
     computed: {
       filteredCities: function () {
         let vm = this;
-        let filtered = vm.brackets.map( () => []);
+        let filtered = vm.brackets.map(() => []);
 
         for (let city of vm.results) {
           for (let i in vm.brackets) {
             i = parseInt(i);
 
-            if((i + 1) === vm.brackets.length) {
+            if ((i + 1) === vm.brackets.length) {
               if (city.cost >= vm.brackets[i]) {
                 filtered[i].push(city)
               }
@@ -93,6 +105,15 @@
       this.$on('fuseResultsUpdated', (results) => {
         this.results = results;
       });
+      this.$on('updatedSearch', (str) => {
+        if (str.length === 0) {
+          this.$emit('hasSearchStr', false);
+          this.searched = false;
+        } else {
+          this.$emit('hasSearchStr', true);
+          this.searched = true;
+        }
+      });
     },
 
     data: function () {
@@ -100,6 +121,8 @@
         brackets: [100, 200, 300, 400, 500, 600, 700, 800, 900, 1000, 1250, 1500, 2000, 3000],
         keys: ['area', 'continent', 'country'],
         results: [],
+        isFixed: false,
+        searched: false,
       };
     },
 
@@ -107,6 +130,21 @@
       shouldRenderBracket: function (i) {
         return this.filteredCities[i].length !== 0;
       },
+      handleScroll: _throttle(function () {
+        let vm = this;
+        let scrollPos = window.scrollY;
+        let contPos = document.getElementById('cities-cont').offsetTop;
+        if (scrollPos >= contPos) {
+          vm.isFixed = true;
+        } else {
+          vm.isFixed = false;
+        }
+      }, 5),
+    },
+
+    mounted: function () {
+      this.$refs.searchBox.$el.focus();
+      window.addEventListener('scroll', this.handleScroll);
     },
 
     props: {
@@ -120,72 +158,90 @@
 
 
 <style scoped lang="scss">
-  $colors-background:  #ffe17e #b66cf7 #00f18d #ff6fc5 #01af46
-                       #b596ff #5ba300 #208fff #d9fc6b #ffa3c9
-                       #75fbff #e05f3d #00adae #b9728b #95a872;
+  $colors-background: #ffe17e #b66cf7 #00f18d #ff6fc5 #01af46 #b596ff #5ba300 #208fff #d9fc6b #ffa3c9 #75fbff #e05f3d #00adae #b9728b #95a872;
 
   .toolbar {
-    margin: 1rem 0;
-    padding: 0;
+    padding: 0.5rem 0;
+    margin: 0;
+    height: 3.5rem;
+
+    &.is-fixed {
+      position: fixed;
+      width: 100%;
+      top: 0;
+      left: 0;
+      background: #FAFAFC;
+      z-index: 10;
+    }
 
     .searchbox {
       width: 100%;
-      padding: 0.6rem 0.9rem;
+      padding: 0.3rem 0.5rem;
       border: 0;
-      border-radius: 0.4rem;
-      outline: none;
+      border-bottom: 0.1rem solid;
 
+      outline: none;
       font-family: 'Roboto', Helvetica, Arial, sans-serif;
       font-size: 1.2rem;
       line-height: 1.5rem;
-      color: #797C86;
-      box-shadow: 0 4px 12px -2px rgba(107, 117, 161, 0.2);
+      background: #FAFAFC;
+      color: #3c3c44;
+
+      &::placeholder {
+        color: #797C86;
+      }
 
     }
   }
 
-  .bracket-container {
-    position: relative;
+  .bracket-wrapper {
+    margin-top: 2rem;
 
-    @each $current-color in $colors-background {
-      $i: index($colors-background, $current-color);
-      &.lvl-#{$i} {
-        background: rgba(red($current-color), green($current-color), blue($current-color), 0.5);
-        color: #424242;
-        padding: 1.3rem 0 1.5rem 0;
+    &.is-fixed {
+      margin-top: 5.5rem;
+    }
+
+    .bracket-container {
+      @each $current-color in $colors-background {
+        $i: index($colors-background, $current-color);
+        &.lvl-#{$i} {
+          background: rgba(red($current-color), green($current-color), blue($current-color), 0.5);
+          color: #424242;
+          padding: 1.3rem 0 1.5rem 0;
+        }
       }
-    }
 
-    .bracket-price {
-      text-align: left;
-      margin: 0;
-      padding: 0;
-    }
-
-    .city {
-      @media (max-width: 576px) {
+      .bracket-price {
         text-align: center;
+        margin: 0;
+        padding: 0;
       }
 
-      .title {
-        padding-bottom: 0.3rem;
-
-        .area {
-          font-size: 1.4rem;
-          padding-right: 0.3rem;
+      .city {
+        @media (max-width: 576px) {
+          text-align: center;
         }
-        .cost {
-          font-size: 0.8rem;
-          font-weight: 300;
-          opacity: 0.8;
+
+        .title {
+          padding-bottom: 0.3rem;
+
+          .area {
+            font-size: 1.4rem;
+            padding-right: 0.3rem;
+          }
+          .cost {
+            font-size: 0.8rem;
+            font-weight: 300;
+            opacity: 0.8;
+          }
         }
-      }
 
-      .location {
-        font-size: 0.9rem;
-        opacity: 0.6;
+        .location {
+          font-size: 0.9rem;
+          opacity: 0.6;
 
-        .country {
+          .country {
+          }
         }
       }
     }
