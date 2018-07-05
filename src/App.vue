f2b56d0be2b990112fe67a1997e15d40adeee56d
treipatru <andrei@planet34.org>
2018-07-05 20:12:14 +0200

    Indenting, updated styles

--- src/App.vue
+++ src/App.vue
@@ -5,27 +5,32 @@
 </template>
 
 <script>
-import Cities from './components/Cities.vue';
-import json from './json/data';
+  import Cities from './components/Cities.vue';
+  import json from './json/data';
 
-export default {
-  name: 'app',
-  components: {
-    Cities,
-  },
-  data: function () {
-    return {
-      citiesData: json.data,
-    };
-  },
-};
+  export default {
+    name: 'app',
+    components: {
+      Cities,
+    },
+    data: function () {
+      return {
+        citiesData: json.data,
+      };
+    },
+  };
 </script>
 
 <style lang="scss">
-#app {
-  font-family: 'Roboto', Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  color: #424242;
-}
+  body {
+    background: #FAFAFC;
+    margin: 0;
+  }
+
+  #app {
+    font-family: 'Roboto', Helvetica, Arial, sans-serif;
+    -webkit-font-smoothing: antialiased;
+    -moz-osx-font-smoothing: grayscale;
+    color: #424242;
+  }
 </style>
