68df00a77042011f2a9a263bb2d6fc64eed23fff
treipatru <andrei@planet34.org>
2018-07-06 16:05:33 +0200

    Added vue-sticky

--- src/main.js
+++ src/main.js
@@ -1,9 +1,11 @@
 import Vue from 'vue';
 import App from './App.vue';
 import VueFuse from 'vue-fuse';
+import Sticky from 'vue-sticky-directive';
 
 Vue.config.productionTip = false;
 Vue.use(VueFuse);
+Vue.use(Sticky);
 
 new Vue({
   render: h => h(App),
